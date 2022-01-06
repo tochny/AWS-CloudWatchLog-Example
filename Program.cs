ca1116920207c74e5d7d2fb45700e89c7cbed73b
tochny <chuheng.chih@icloud.com>
2022-01-06 22:47:01 +0800

    feat: add cloudwatch logs example

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+using NLog;
+using NLog.Targets;
+using NLog.Config;
+
+using NLog.AWS.Logger;
+
+namespace TestLog
+{
+    class CloudWatchTest
+    {
+        static void Main(string[] args)
+        {
+            ConfigureNLog();
+
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Check the AWS Console CloudWatch Logs console in us-west-2");
+            logger.Info("to see messages in the log streams for the");
+        }
+
+        private static void ConfigureNLog()
+        {
+            var config = new LoggingConfiguration();
+
+            var consoleTarget = new ColoredConsoleTarget{
+                Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss.fff} [${level:uppercase=true}] ${message} ${exception:format=tostring}"
+            };
+            config.AddTarget("console", consoleTarget);
+
+            var awsTarget = new AWSTarget()
+            {
+                LogGroup = "testGroup",
+                Region = "us-west-2",
+                Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss.fff} [${level:uppercase=true}] ${message} ${exception:format=tostring}",
+                LogStreamNamePrefix = ""
+            };
+            config.AddTarget("aws", awsTarget);
+
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Info, consoleTarget));
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, awsTarget));
+
+            LogManager.Configuration = config;
+        }
+    }
+}
